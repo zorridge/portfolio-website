774b3ba294cd9c3ab9dee4edd5d7baca039a7d89
Zhou Ziheng <zhou.ziheng@outlook.com>
2023-05-21 01:54:35 +0800

    add navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import './App.css';
 
+import Header from './components/header/Header';
+
 function App() {
-    return <h1>Hello world!</h1>;
+    return (
+        <>
+            <Header />
+        </>
+    );
 }
 
 export default App;
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,68 @@
+import React, { useState } from 'react';
+import './header.css';
+
+const Header = () => {
+    const [isMenuShown, setIsMenuShown] = useState(false);
+
+    const menuToggleHandler = () => {
+        setIsMenuShown(prevState => !prevState);
+    };
+
+    return (
+        <header className='header'>
+            <nav className='nav container'>
+                <a href='index.html' className='nav__logo'>
+                    Zhou
+                </a>
+                <div
+                    className={
+                        isMenuShown ? 'nav__menu show-menu' : 'nav__menu'
+                    }>
+                    <ul className='nav__list grid'>
+                        <li className='nav__item'>
+                            <a href='#home' className='nav__link active-link'>
+                                <i className='uil uil-home nav__icon' />
+                                Home
+                            </a>
+                        </li>
+                        <li className='nav__item'>
+                            <a href='#about' className='nav__link'>
+                                <i className='uil uil-user nav__icon' />
+                                About
+                            </a>
+                        </li>
+                        <li className='nav__item'>
+                            <a href='#skills' className='nav__link'>
+                                <i className='uil uil-file-alt nav__icon' />
+                                Skills
+                            </a>
+                        </li>
+                        <li className='nav__item'>
+                            <a href='#portfolio' className='nav__link'>
+                                <i className='uil uil-scenery nav__icon' />
+                                Portfolio
+                            </a>
+                        </li>
+                        <li className='nav__item'>
+                            <a href='#contact' className='nav__link'>
+                                <i className='uil uil-message nav__icon' />
+                                Contact
+                            </a>
+                        </li>
+                    </ul>
+
+                    <i
+                        className='uil uil-times nav__close'
+                        onClick={menuToggleHandler}
+                    />
+                </div>
+
+                <div className='nav__toggle' onClick={menuToggleHandler}>
+                    <i className='uil uil-apps' />
+                </div>
+            </nav>
+        </header>
+    );
+};
+
+export default Header;
