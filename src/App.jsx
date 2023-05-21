5ead881fc0df969a902edd1c7b17c5f575c461ce
Zhou Ziheng <zhou.ziheng@outlook.com>
2023-05-21 19:37:59 +0800

    add background stars

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css';
 
+import Stars from './components/stars/Stars';
 import Header from './components/header/Header';
 import Home from './components/home/Home';
 
 function App() {
     return (
         <>
+            <Stars />
             <Header />
             <main className='main'>
                 <Home />
---
+++ src/components/stars/Stars.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './stars.css';
+
+const Stars = () => {
+    return (
+        <>
+            <div className='stars-1'></div>
+            <div className='stars-2'></div>
+            <div className='stars-3'></div>
+        </>
+    );
+};
+
+export default Stars;
